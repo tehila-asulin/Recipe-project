d36870011290f46b608d1dec41ec626a731734ef
tehila-asulin <a0548503767@gmail.com>
2025-02-26 23:10:25 +0200

    getAllRecipes

--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const {createRecipe,deleteRecipe,updateRecipe} = require('../controllers/recipeController');
+const {createRecipe,deleteRecipe,updateRecipe,getAllRecipes} = require('../controllers/recipeController');
 
 const router = express.Router();
 const User = require("../models/users");
 router.post('/', createRecipe);
 router.delete('/:id', deleteRecipe);
 router.put('/:id', updateRecipe);
-
+router.get('/recipes',getAllRecipes)
 module.exports = router;
